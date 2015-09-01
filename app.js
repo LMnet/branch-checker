d0c53aff2d899d92e31ab8860a811ff1eb17f950
LMnet <lmnet89@gmail.com>
2015-09-01 11:50:21 +0600

    Create tmp dir

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var GitHubApi = require("github");
 var utils = require("./utils.js");
 var http = require("http");
 
+process.stdin.resume();
+process.on('exit', utils.cleanup.bind(utils));
+process.on('SIGINT', utils.cleanup.bind(utils));
+process.on('uncaughtException', utils.cleanup.bind(utils));
+
+utils.createRootTmpDir();
+
 var github = new GitHubApi({
     version: "3.0.0",
     debug: true
@@ -14,18 +21,6 @@ github.authenticate({
 
 http.createServer(function(request, response) {
     if (request.method == 'POST') {
-        var rawBody = '';
-        request.on('data', function (chunk) {
-            rawBody += chunk;
-        });
-        request.on('end', function () {
-            var body = JSON.parse(rawBody);
-            console.log(body.zen);
-            console.log(body.hook.url);
-
-            response.writeHead(200, {"Content-Type": "text/plain"});
-            response.write("Hello World");
-            response.end();
-        });
+        utils.githubEventHandler(request, response)
     }
 }).listen(4567);
--- utils.js
+++ utils.js
@@ -1,6 +1,36 @@
 var fs = require("fs");
+var randomstring = require("randomstring");
 
 var utils = {
+
+    rootTmpDir: null,
+
+    createRootTmpDir: function() {
+        var tmpDirName = "/tmp/branch-checker-" + randomstring.generate(5);
+        fs.mkdirSync(tmpDirName);
+        this.rootTmpDir = tmpDirName;
+    },
+
+    _deleteDir: function(path) {
+        var self = this;
+        if (fs.existsSync(path)) {
+            fs.readdirSync(path).forEach(function(file){
+                var curPath = path + "/" + file;
+                if (fs.lstatSync(curPath).isDirectory()) {
+                    self._deleteDir(curPath);
+                } else {
+                    fs.unlinkSync(curPath);
+                }
+            });
+            fs.rmdirSync(path);
+        }
+    },
+
+    cleanup: function() {
+        this._deleteDir(this.rootTmpDir);
+        process.exit();
+    },
+
     getToken: function() {
         var token;
         try {
@@ -10,7 +40,22 @@ var utils = {
             process.exit(1);
         }
         return token;
-    }
+    },
+
+    githubEventHandler: function(request, response) {
+        var rawBody = '';
+        request.on('data', function (chunk) {
+            rawBody += chunk;
+        });
+        request.on('end', function () {
+            var body = JSON.parse(rawBody);
+
+            response.writeHead(200);
+            response.end();
+        });
+    },
+
+
 };
 
 module.exports = utils;
