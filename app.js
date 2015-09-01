28a5da07d5e9187c4e7cd6444807fe6f6b77a817
LMnet <lmnet89@gmail.com>
2015-09-01 12:14:47 +0600

    Fixes

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ var utils = new Utils();
 process.stdin.resume();
 process.on('exit', utils.cleanup.bind(utils));
 process.on('SIGINT', utils.cleanup.bind(utils));
-process.on('uncaughtException', utils.cleanup.bind(utils));
+process.on('uncaughtException', function(err) {
+    console.error(err.stack);
+    utils.cleanup();
+});
 
 utils.createRootTmpDir();
 utils.authenticate();
 
 http.createServer(function(request, response) {
     if (request.method == 'POST') {
-        utils.githubEventHandler(request, response)
+        if (request.headers["x-github-event"] === "pull_request") {
+            console.log("Receive GitHub pull request event");
+            utils.githubEventHandler(request, response);
+        }
     }
 }).listen(4567);
